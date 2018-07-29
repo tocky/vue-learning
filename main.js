7018ffda714f21af6d8cf43d5e851a02ba26d031
Shin Tokiwa <shin.tokiwa@gmail.com>
2018-07-29 19:12:27 +0900

    Chapter 1 ends

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 var app = new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!',
+    list: ['apple', 'banana', 'strawberry'],
+    show: true
+  },
+  methods: {
+    handleClick: function(event) {
+      alert(event.target);
+    }
+  },
+  created: function() {
+    console.log('created');
+  },
+  mounted: function() {
+    console.log('mounted');
+  }
 })
